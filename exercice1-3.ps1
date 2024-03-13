ca702abe40c93dc17784a8056796c953a7416f3d
Pierre-LouisGuinoiseau <pierrelouis.guinoiseau1@gmail.com>
2024-03-13 10:54:13 +0100

    4eme commit

---
+++ exercice1-3.ps1
@@ -0,0 +1,8 @@
+﻿#==========================================================
+# NAME exercice 1-3
+# AUTHOR : Guinoiseau Pierre-Louis
+# DATE : 13/03/2024
+# VERSION 1.1
+#==========================================================
+# Afficher les svchost 
+Get-process -ProcessName svchost | Format-table Id, Name, Description
---
+++ exercice1-4.ps1
@@ -0,0 +1,8 @@
+﻿#==========================================================
+# NAME exercice 1-4
+# AUTHOR : Guinoiseau Pierre-Louis
+# DATE : 13/03/2024
+# VERSION 1.1
+#==========================================================
+$Name = Read-Host "entrer le process"
+Get-process -ProcessName *$Name* | Format-table Id, Name, description
